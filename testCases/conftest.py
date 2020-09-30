de3be242f8ff81972db3bf02c8d693ab9370aa14
Shashank <s_shashank@outlook.com>
2020-09-30 23:45:56 +0530

    updated chrome options

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -12,8 +12,10 @@ def setup(browser):
     elif browser == 'firefox':
         driver = webdriver.Firefox()
     else:
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
         driver = webdriver.Chrome(
-            executable_path="C:/Users/shash/Documents/drivers/chromedriver.exe")
+            executable_path="C:/Users/shash/Documents/drivers/chromedriver.exe", options=options)
     return driver
 
 
